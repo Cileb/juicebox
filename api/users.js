c19a2d639a8c38e4ed2d73bfaec107be7b6dfa66
Cileb <Cileb@users.noreply.github.com>
2022-10-01 10:09:24 -0400

    Final Test 10

--- api/users.js
+++ api/users.js
@@ -34,7 +34,7 @@ usersRouter.post("/login", async (req, res, next) => {
     const user = await getUserByUsername(username);
 
     if (user && user.password == password) {
-      let token = jwt.sign(user, JWT_SECRET);
+      let token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "5h" });
       res.send({ message: "You're logged in!", token });
     } else {
       next({
